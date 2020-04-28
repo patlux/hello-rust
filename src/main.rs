fd49bdc2b987f91197fa8ab4fe73ac7e68a04058
Patrick Wozniak <email@patwoz.de>
2020-04-28 14:22:23 +0200

    guessing game tutorial
    https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,30 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    // In general, the `{}` will be automatically replaced with any
-    // arguments. These will be stringified.
-    println!("{} days", 31);
+  println!("Hey Patrick!");
+  let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    // Without a suffix, 31 becomes an i32. You can change what type 31 is
-    // by providing a suffix.
+  loop {
+    println!("Please input your guess");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Expected a guess");
+    print!("You guessed: {}", guess);
 
-    // There are various optional patterns this works with. Positional
-    // arguments can be used.
-    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
+    let re = guess.cmp(&secret_number);
 
-    // As can named arguments.
-    println!("{subject} {verb} {object}",
-             object="the lazy dog",
-             subject="the quick brown fox",
-             verb="jumps over");
-
-    // Special formatting can be specified after a `:`.
-    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
-
-    // You can right-align text with a specified width. This will output
-    // "     1". 5 white spaces and a "1".
-    println!("{number:>width$}", number=1, width=6);
-
-    // You can pad numbers with extra zeroes. This will output "000001".
-    println!("{number:>0width$}", number=1, width=6);
-
-    // Rust even checks to make sure the correct number of arguments are
-    // used.
-    println!("My name is {0}, {1} {0}", "Bond", "rofl");
-    // FIXME ^ Add the missing argument: "James"
-
-    // Create a structure named `Structure` which contains an `i32`.
-    #[allow(dead_code)]
-    struct Structure(i32);
-
-    // However, custom types such as this structure require more complicated
-    // handling. This will not work.
-    // println!("This struct `{}` won't print...", Structure(3));
-    // FIXME ^ Comment out this line.
-
-    let pi = 3.141592;
-    println!("{pi:.2}", pi=pi);
+    match re {
+      Ordering::Equal => {
+        println!("Correct!");
+        break;
+      }
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+    }
+  }
 }
